8ec966fa7171ba9c6ae57ea356742a5907f2ca75
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2020-11-15 10:52:05 +0600

    Added repository user interfaces and classes

---
+++ Repository/CompanyRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
+    {
+        public CompanyRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
---
+++ Repository/EmployeeRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Contracts;
+using Entities;
+using Entities.Models;
+
+namespace Repository
+{
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
+    {
+        public EmployeeRepository(RepositoryContext repositoryContext)
+            : base(repositoryContext)
+        {
+        }
+    }
+}
