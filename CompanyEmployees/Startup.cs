f65ca31f2d0b8da226740f85b006340e125d36ea
Mohammad Arfizur Rahman <arfizrahman0@gmail.com>
2020-11-18 14:31:31 +0600

    Implemented custom CSV formatter

--- CompanyEmployees/Startup.cs
+++ CompanyEmployees/Startup.cs
@@ -44,7 +44,8 @@ namespace CompanyEmployees
             {
                 config.RespectBrowserAcceptHeader = true;
                 config.ReturnHttpNotAcceptable = true;
-            }).AddXmlDataContractSerializerFormatters();
+            }).AddXmlDataContractSerializerFormatters()
+                .AddCustomCSVFormatter();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
